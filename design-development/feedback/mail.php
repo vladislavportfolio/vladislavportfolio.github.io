77d5360e3ee87c58276e153fdf9ed8d624a6751e
design-development <course.volodko@mail.ru>
2018-01-13 16:05:31 +0300

    design-development

--- design-development/feedback/mail.php
+++ design-development/feedback/mail.php
@@ -10,3 +10,4 @@ $message = "Имя: $name \nЭлектронная почта: $email \nТекс
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
