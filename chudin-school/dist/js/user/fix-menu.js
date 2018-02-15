685e94b56da831be88b9bf68867d58c2320ca368
design-development <course.volodko@mail.ru>
2018-02-15 16:29:04 +0300

    chudin-school

---
+++ chudin-school/dist/js/user/fix-menu.js
@@ -0,0 +1,22 @@
+     var h_hght = 110; // высота шапки
+        var h_mrg = 0;    // отступ когда шапка уже не видна
+
+        $(function(){
+        var elem = $('#top_nav');
+        var top = $(this).scrollTop();
+
+        if(top > h_hght){
+            elem.css('top', h_mrg);
+        }           
+
+        $(window).scroll(function(){
+            top = $(this).scrollTop();
+
+            if (top+h_mrg < h_hght) {
+                elem.css('top', (h_hght-top));
+            } else {
+                elem.css('top', h_mrg);
+            }
+        });
+
+    });
